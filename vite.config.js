379c8fd6538412c2214a78e5ed8953ca3d967ed9
Mariele Vieira da Silva <mariele.vieira@aguasdejoinville.com.br>
2026-06-15 16:09:10 -0300

    tentativa 1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/portfolio/', // Substitua pelo nome do seu repositório
+  base: '/portfolio', // Substitua pelo nome do seu repositório
 })
